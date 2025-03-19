5807582b8a24fe498973a2830a476519316f0885
AlissonSimon <alissonsimon21@gmail.com>
2025-03-19 19:25:26 -0300

    Atualização de Estilo

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.scss';
 
 function List () {
 const tarefas = [ 
@@ -14,11 +15,11 @@ const tarefas = [
 }
 ]
 return (
-    <aside>
+    <aside className='listaTarefas'>
         <h2>Estudos do dia</h2>
         <ul>
             {tarefas.map((item, index) => (
-                <li key={index}>
+                <li key={index} className='item'>
                     <h3>
                         {item.tarefa}
                     </h3>
